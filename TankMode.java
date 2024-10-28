26a978de6d08c50da9939e661871f784814a372e
JavaJade <167595374+JavaJade@users.noreply.github.com>
2024-10-28 10:25:19 -0700

    Create TankMode.java

---
+++ TankMode.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Tank Mode")
+
+public class TankMode {
+  
+    @Override
+    public void init(){
+        
+    }
+    
+    @Override
+    public void loop(){
+        
+    }
+    
+    
+}
